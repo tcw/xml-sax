42253ee61f255ca8a6d5337914b0fc774192004b
tcw <tcwgit@gmail.com>
2012-10-18 23:38:17 +0200

    First step done

--- spec/xml_sax/core_spec.clj
+++ spec/xml_sax/core_spec.clj
@@ -1,11 +1,25 @@
 (ns xml-sax.core-spec
-(:use
-  speclj.core
-  xml-sax.core))
+  (:use speclj.core
+        xml-sax.core))
+
+(defn get-attrs [atts]
+  (into {} (for [i (range (.getLength atts))]
+             [(keyword (.getQName atts i))
+              (.getValue atts i)])))
+
+(defn get-string [ch start length]
+  (let [st (String. ch start length)]
+    (when (seq (.trim st)))))
+
 
 (describe "Sax parse xml"
 
-  (it "print shit"
-    (should= "Hello, foo" (sax-parse "shit.xml"))))
+  (it "parses xml with sax in a xpath forward manor"
+    (let [counter (atom 0)]
+;      (pull-xml "shit.xml" "staff" (fn [elem] (swap! counter inc)))
+      (pull-xml "/home/tom/XML/standard.xml" "site/regions/africa" (fn [elem]
+                                                      (swap! counter inc)))
+       (should= 21750 @counter)))
+  )
 
 (run-specs)
--- src/xml_sax/core.clj
+++ src/xml_sax/core.clj
@@ -4,19 +4,34 @@
            (javax.xml.parsers SAXParserFactory)
            (java.io Reader)))
 
-(defn get-attrs [atts]
-  (into {} (for [i (range (.getLength atts))]
-             [(keyword (.getQName atts i))
-              (.getValue atts i)])))
-
-(defn sax-parse [s]
+(defn sax-parse [s sfn efn cfn]
   (let [s (if (instance? Reader s) (InputSource. s) s)]
-    (.. SAXParserFactory newInstance newSAXParser (parse s (proxy [DefaultHandler] []
-                    (startElement [uri local-name q-name #^Attributes atts]
-                        (println (str "start -> " (keyword q-name) (get-attrs atts))))
-                    (endElement [uri local-name q-name]
-                      (println (str "end -> " (keyword q-name))))
-                    (characters [ch start length]
-                      (let [st (String. ch start length)]
-                        (when (seq (.trim st))
-                          (println (str "chars -> " st))))))))))
+    (.. SAXParserFactory
+      newInstance
+      newSAXParser
+      (parse s (proxy [DefaultHandler] []
+                 (startElement [uri local-name q-name #^Attributes atts]
+                   (sfn uri local-name q-name atts))
+                 (endElement [uri local-name q-name]
+                   (efn uri local-name q-name))
+                 (characters [ch start length]
+                   (cfn ch start length)))))))
+
+
+(defn pull-xml [source fxpath f]
+  (let [start-elem (atom false) xml-elem (atom [])]
+    (sax-parse source
+      (fn [uri local-name q-name #^Attributes atts]
+        (when (= fxpath q-name) (swap! start-elem not))
+        (when @start-elem (swap! xml-elem conj (str "<" q-name ">")))
+        ),
+      (fn [uri local-name q-name]
+        (when @start-elem (swap! xml-elem conj (str "</" q-name ">")))
+        (when (= fxpath q-name)
+          (swap! start-elem not)
+            (f @xml-elem)
+          (reset! xml-elem []))
+        ),
+      (fn [ch start length]
+        (when @start-elem (swap! xml-elem conj (.trim (String. ch start length))))
+        ))))
